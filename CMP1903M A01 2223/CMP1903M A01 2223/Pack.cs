fa4439a3e54d9d3840b8847d61e9062523f9069c
Alfie7G <127167920+Alfie7G@users.noreply.github.com>
2023-03-07 04:54:22 +0000

    commit

--- CMP1903M A01 2223/CMP1903M A01 2223/Pack.cs
+++ CMP1903M A01 2223/CMP1903M A01 2223/Pack.cs
@@ -8,11 +8,20 @@ namespace CMP1903M_A01_2223
 {
     class Pack
     {
-        List<Card> pack;
+        List<Card> pack = new List<Card>();
 
         public Pack()
         {
             //Initialise the card pack here
+            foreach (int suit in Enumerable.Range(1,4))
+            {
+                foreach (int value in Enumerable.Range(1,12))
+                {
+                    pack.Add(new card(suit, value));
+                }
+
+            }
+
         }
 
         public static bool shuffleCardPack(int typeOfShuffle)
