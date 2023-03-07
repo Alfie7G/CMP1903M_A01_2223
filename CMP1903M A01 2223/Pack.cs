50f2552bc928a7bba5ba6e8c6e0d0e774cd90c29
Alfie7G <127167920+Alfie7G@users.noreply.github.com>
2023-03-07 05:54:11 +0000

    Add files via upload

---
+++ CMP1903M A01 2223/Pack.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMP1903M_A01_2223
+{
+    class Pack
+    {
+        List<Card> pack = new List<Card>();
+
+        public Pack()
+        {
+            //Initialise the card pack here
+            foreach (int suit in Enumerable.Range(1,4))
+            {
+                foreach (int value in Enumerable.Range(1,12))
+                {
+                    pack.Add(new card(suit, value));
+                }
+
+            }
+            foreach(int item in pack)
+            {
+            Console.WriteLine(item);
+            }
+        }
+
+        public static bool shuffleCardPack(int typeOfShuffle)
+        {
+            //Shuffles the pack based on the type of shuffle
+            return false;
+        }
+        public static Card deal()
+        {
+            //Deals one card
+            return null;
+        }
+        public static List<Card> dealCard(int amount)
+        {
+            //Deals the number of cards specified by 'amount'
+            return null;
+        }
+    }
+}
