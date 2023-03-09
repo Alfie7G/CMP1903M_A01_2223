3c094146d1e4003829f1ec169f37630a28bf2473
Alfie7G <127167920+Alfie7G@users.noreply.github.com>
2023-03-09 01:11:29 +0000

    Update Program.cs

--- CMP1903M A01 2223/Program.cs
+++ CMP1903M A01 2223/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,12 +7,29 @@ using System.Threading.Tasks;
 
 namespace CMP1903M_A01_2223
 {
+    //Main program
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("fjgalsd");
             Pack deck = new Pack();
+            
+            Console.WriteLine("Enter shuffle type: ");
+            Console.WriteLine("1: fisheryates, 2: riffle, 3: no shuffle");
+            string shuffle_type = Console.ReadLine();
+            
+            int shuffle_int = Int32.Parse(shuffle_type);
+            
+            deck.shuffleCardPack(shuffle_int);
+
+            Console.WriteLine("Enter number of cards to be dealt");
+            string amount_of_cards = Console.ReadLine();
+
+            int amount_of_cards_int = Int32.Parse(amount_of_cards);
+
+            deck.dealCard(amount_of_cards_int);
+
+//            Testing testing = new Testing();                           //Used for testing
         }
     }
 }
